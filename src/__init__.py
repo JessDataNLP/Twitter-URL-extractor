0cf90da00282bd4f1d18d08a5fe6a2f1e553ae70
Jessica <58152169+JessDataNLP@users.noreply.github.com>
2024-02-28 15:24:17 +0100

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,3 @@
+# Import the main functions from the modules
+from .expand_urls import process_urls, expand_url
+from .extract_data import extract_website_name, classify_url, extract_telegram_account, get_video_title
